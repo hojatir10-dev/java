d4ebb04ce51b76cdc9c564c6035e89272a5b3525
hojatir10-dev <hojatir10@gmail.com>
2025-12-04 10:57:19 -0800

    Create recive.java

---
+++ recive.java
@@ -0,0 +1,20 @@
+package com.example.final_app;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+
+        Toast.makeText(context, "Internet Connected: " + isConnected, Toast.LENGTH_SHORT).show();
+    }
+}
